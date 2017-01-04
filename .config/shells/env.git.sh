8cae64d2f0313f65e320485454ce1a2ed70ae89a
Henrik Farre <hfar@tv2.dk>
2017-01-04 10:31:56 +0100

    Configure git envs

---
+++ .config/shells/env.git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $HOST == 'workstation' ]]; then
+  export GIT_COMMITTER_EMAIL="hfar@tv2.dk"
+  export GIT_AUTHOR_EMAIL="hfar@tv2.dk"
+  export GIT_AUTHOR_NAME="Henrik Farre"
+  export GIT_COMMITTER_NAME="Henrik Farre"
+fi
