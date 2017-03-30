523932a6768896e90e6bc95a07599ef1b773a514
Henrik Farre <hfar@tv2.dk>
2017-03-30 13:03:00 +0200

    Fix check for ruby

--- .config/shells/env.sh
+++ .config/shells/env.sh
@@ -50,7 +50,7 @@ if [[ -d ${HOME}/bin ]]; then
 fi
 
 # Check if gems module is installed
-if which ruby >/dev/null && which gem >/dev/null; then
+if which ruby &>/dev/null && which gem &>/dev/null; then
   PATH="${PATH}:$(ruby -rubygems -e 'puts Gem.user_dir')/bin"
 fi
 
