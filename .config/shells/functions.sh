434e3f73360d24cf078ea60e25239b879df4e52e
Henrik Farre <hfar@tv2.dk>
2017-11-08 09:33:00 +0100

    support mac, echo ip

--- .config/shells/functions.sh
+++ .config/shells/functions.sh
@@ -303,7 +303,12 @@ function switch-dns() {
 
 function check_if_ip_is_free() {
   local IP=$1
-  ping -c1 -w2 "${IP}"
+  echo "================ $IP ================"
+  if [[ $PLATFORM == 'Darwin' ]]; then
+    ping -c1 -t2 "${IP}"
+  else
+    ping -c1 -w2 "${IP}"
+  fi
   wget -S -T 2 --tries 1 "http://${IP}" -O /dev/null
   wget -S -T 2 --tries 1 "http://${IP}:8080" -O /dev/null
   wget -S -T 2 --tries 1 "https://${IP}" -O /dev/null
