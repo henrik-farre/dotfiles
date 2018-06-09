cac1a21c1e8ecf2cb80f98ecec65060a7f8b41e2
Henrik Farre <henrik@rockhopper.dk>
2018-06-09 17:25:34 +0200

    Support multiple hosts

--- .config/devilspie2/devilspie2.lua
+++ .config/devilspie2/devilspie2.lua
@@ -3,10 +3,18 @@
 -- debug_print("Window Name: " .. get_window_name());
 -- debug_print("Application name: " .. get_application_name())
 
+local socket = require "socket"
+local hostname = socket.dns.gethostname();
+
 -- I want my Xfce4-terminal to the right on the second screen of my two-monitor
 -- setup. (String comparison are case sensitive, please note this when
 -- creating rule scripts.)
 if (get_application_name() == "neovim") then
-  xy(2881, 0);
+  if(hostname == "firehouse") then
+    xy(0, 0);
+  elseif (hostname == "workstation") then
+    xy(2881, 0);
+  end
+
   maximize();
 end
