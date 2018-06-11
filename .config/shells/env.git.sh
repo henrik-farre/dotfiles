fa888ae51662d1445b7b112f9c968d09d862200a
Henrik Farre <henrik@rockhopper.dk>
2018-06-11 13:46:47 +0200

    Disable export of GIT_* vars

--- .config/shells/env.git.sh
+++ .config/shells/env.git.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [[ $(hostname) == 'workstation' ]]; then
-  export GIT_COMMITTER_EMAIL="hfar@tv2.dk"
-  export GIT_AUTHOR_EMAIL="hfar@tv2.dk"
-  export GIT_AUTHOR_NAME="Henrik Farre"
-  export GIT_COMMITTER_NAME="Henrik Farre"
-fi
+# Use includeIf in ~/.gitconfig instead
+# if [[ $(hostname) == 'workstation' ]]; then
+#   export GIT_COMMITTER_EMAIL="hfar@tv2.dk"
+#   export GIT_AUTHOR_EMAIL="hfar@tv2.dk"
+#   export GIT_AUTHOR_NAME="Henrik Farre"
+#   export GIT_COMMITTER_NAME="Henrik Farre"
+# fi
