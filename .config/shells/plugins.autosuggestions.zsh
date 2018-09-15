70e3ec156dddae3648d1afc73bed35d9d3470893
Henrik Farre <henrik@rockhopper.dk>
2018-09-15 14:39:00 +0200

    Enable autosuggestions

---
+++ .config/shells/plugins.autosuggestions.zsh
@@ -0,0 +1,4 @@
+if [[ -e /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+  export ZSH_AUTOSUGGEST_USE_ASYNC=true
+  source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
