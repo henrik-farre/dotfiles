2e8da134ccbba472deea83c83d545eeed34c5713
Henrik Farre <henrik@rockhopper.dk>
2018-09-15 15:11:41 +0200

    Clean fzf plugin settings up

--- .config/shells/plugins.fzf.zsh
+++ .config/shells/plugins.fzf.zsh
@@ -2,6 +2,7 @@
 # FZF
 # From https://github.com/junegunn/fzf
 FZF_KEYBINDINGS_PATH=/usr/share/fzf/key-bindings.zsh
+FZF_COMPLETION_PATH=/usr/share/fzf/completion.zsh
 if [[ $PLATFORM == 'Darwin' ]]; then
   FZF_KEYBINDINGS_PATH=/usr/local/opt/fzf/shell/key-bindings.zsh
 fi
@@ -9,7 +10,7 @@ fi
 # Use -e if symlink (locally installed)
 if [[ -f $FZF_KEYBINDINGS_PATH ]]; then
   # System installed:
-  . $FZF_KEYBINDINGS_PATH
+  source $FZF_KEYBINDINGS_PATH
   # Local installed:
   # source ~/.fzf.zsh
 
@@ -24,68 +25,13 @@ if [[ -f $FZF_KEYBINDINGS_PATH ]]; then
 
   # Else fzf will not split using tmux
   export FZF_TMUX=1
+fi
 
-  #
-  # fe [FUZZY PATTERN] - Open the selected file with the default editor
-  #   - Bypass fuzzy finder if there's only one match (--select-1)
-  #   - Exit if there's no match (--exit-0)
-  fe() {
-    local file
-    file=$(fzf --query="$1" --select-1 --exit-0)
-    [ -n "$file" ] && ${EDITOR:-vim} "$file"
-  }
-
-  # fd - cd to selected directory
-  fd() {
-    local dir
-    dir=$(find ${1:-*} -path '*/\.*' -prune \
-                    -o -type d -print 2> /dev/null | fzf +m) &&
-    cd "$dir"
-  }
-
-  # fda - including hidden directories
-  fda() {
-    local dir
-    dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
-  }
-
-  # fh - repeat history
-  fh() {
-    eval $(([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s | sed 's/ *[0-9]* *//')
-  }
-
-  # fkill - kill process
-  fkill() {
-    ps -ef | sed 1d | fzf -m | awk '{print $2}' | xargs kill -${1:-9}
-  }
-
-  # fbr - checkout git branch
-  fbr() {
-    local branches branch
-    branches=$(git branch) &&
-    branch=$(echo "$branches" | fzf +s +m) &&
-    git checkout $(echo "$branch" | sed "s/.* //")
-  }
-
-  # fco - checkout git commit
-  fco() {
-    local commits commit
-    commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
-    commit=$(echo "$commits" | fzf +s +m -e) &&
-    git checkout $(echo "$commit" | sed "s/ .*//")
-  }
-
-  # ftags - search ctags
-  ftags() {
-    local line
-    [ -e tags ] &&
-    line=$(
-      awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
-      cut -c1-80 | fzf --nth=1,2
-    ) && $EDITOR $(cut -f3 <<< "$line") -c "set nocst" \
-                                        -c "silent tag $(cut -f2 <<< "$line")"
-  }
-
-  zle     -N   fzf-git-changed-files
-  bindkey '^F' fzf-git-changed-files
+if [[ -f $FZF_COMPLETION_PATH ]]; then
+  export FZF_COMPLETION_TRIGGER=''
+  source $FZF_COMPLETION_PATH
+  # "setopt vi resets TAB key binding, so unless you've assigned a dedicated key, fuzzy completion will become unavailable."
+  # - https://github.com/junegunn/fzf/wiki/Configuring-fuzzy-completion
+  bindkey '^T' fzf-completion
+  bindkey '^I' $fzf_default_completion
 fi
