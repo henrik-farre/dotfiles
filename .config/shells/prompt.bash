6c0e785d47bf231fcc2873ee46f84075c37358d9
Henrik Farre <henrik@rockhopper.dk>
2018-10-10 10:24:43 +0200

    Highlight hostname based on environment

--- .config/shells/prompt.bash
+++ .config/shells/prompt.bash
@@ -1,7 +1,7 @@
 function render_prompt()
 {
   # Window title
-    local TITLEBAR
+  local TITLEBAR
   case $TERM in
     screen|screen-bce|screen-256color|screen-256color-bce)
       TITLEBAR='\033k\033\0134\033k${HOSTNAME%%.*}:${PWD/$HOME/~}\033\0134'
@@ -12,10 +12,25 @@ function render_prompt()
   esac
   TITLEBAR='\[\033]0;\u:${NEW_PWD}\007\]'
 
+  local ENV_COLOR
+  ENV_COLOR='\033[1;33m'
+  case "$(hostname)" in
+    *.production.* )
+      ENV_COLOR='\033[30m\033[41m'
+      ;;
+    *.backend.* )
+      ENV_COLOR='\033[0;31m'
+      ;;
+    *.dev.* )
+      ENV_COLOR='\033[0;32m'
+      ;;
+  esac
+
+
   if [[ "${USER}" == 'root' ]]; then
     PS1="${TITLEBAR}\[\033[0;31m\]\u@\H\[\033[00m\]:\w> "
   else
-    PS1="${TITLEBAR}\[\033[0;32m\]\H\[\033[00m\]:\w> "
+    PS1="${TITLEBAR}${ENV_COLOR}\H\[\033[00m\]:\w> "
   fi
 
   # export PS1
