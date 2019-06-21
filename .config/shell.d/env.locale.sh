c8765c7f5c879b009908c1b6df2565ce42914f23
Henrik Farre <henrik@rockhopper.dk>
2019-06-21 11:48:49 +0200

    prompt fix for macosx

--- .config/shell.d/env.locale.sh
+++ .config/shell.d/env.locale.sh
@@ -6,3 +6,8 @@ if [[ -z "${LANG}" || -z "${LC_ALL}" ]]; then
   export LANG=da_DK.utf8
   export LC_ALL=da_DK.utf8
 fi
+
+# Fix: cursor is on top of prompt and command is repeated
+if [[ $PLATFORM == 'Darwin' && -z "${LC_CTYPE}" ]]; then
+  export LC_CTYPE=da_DK.UTF-8
+fi
