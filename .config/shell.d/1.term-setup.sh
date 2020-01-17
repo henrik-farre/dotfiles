2c6c154a181c5e3eb06bd40a934b3ca70cfdbd63
Henrik Farre <henrik@rockhopper.dk>
2020-01-17 09:52:24 +0100

    update term setup

--- .config/shell.d/1.term-setup.sh
+++ .config/shell.d/1.term-setup.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 ############################################################
 # Terminal settings
-if [[ "${TERM}" == "rxvt-unicode" ]] && \
-        [[ ! -f /usr/share/terminfo/r/rxvt-unicode ]] && \
-        [[ ! -f ~/.terminfo/r/rxvt-unicode ]] ; then
-    export TERM=rxvt
-fi
-
-if [[ "${TERM}" == "rxvt-256color" ]] && \
-        [[ ! -f /usr/share/terminfo/r/rxvt-256color ]] && \
-        [[ ! -f ~/.terminfo/r/rxvt-256color ]] ; then
-    export TERM=rxvt
-fi
-
-if [[ "${TERM}" == "rxvt-unicode-256color" ]] && \
-        [[ ! -f /usr/share/terminfo/r/rxvt-unicode-256color ]] && \
-        [[ ! -f ~/.terminfo/r/rxvt-unicode-256color ]] ; then
-    export TERM=rxvt
-fi
-
-if [[ "${TERM}" == "xterm" ]]; then
-  export TERM=xterm-256color
-fi
-
-if [[ $TERM == "tmux-256color" && -z $TMUX ]]; then
-  export TERM="screen-256color"
-fi
-
-if [[ $TERM == "screen-256color" && -n $SSH_TTY && ! -e /usr/bin/screen ]]; then
-  export TERM="xterm-256color"
-fi
+#if [[ "${TERM}" == "rxvt-unicode" ]] && \
+#        [[ ! -f /usr/share/terminfo/r/rxvt-unicode ]] && \
+#        [[ ! -f ~/.terminfo/r/rxvt-unicode ]] ; then
+#    export TERM=rxvt
+#fi
+#
+#if [[ "${TERM}" == "rxvt-256color" ]] && \
+#        [[ ! -f /usr/share/terminfo/r/rxvt-256color ]] && \
+#        [[ ! -f ~/.terminfo/r/rxvt-256color ]] ; then
+#    export TERM=rxvt
+#fi
+#
+#if [[ "${TERM}" == "rxvt-unicode-256color" ]] && \
+#        [[ ! -f /usr/share/terminfo/r/rxvt-unicode-256color ]] && \
+#        [[ ! -f ~/.terminfo/r/rxvt-unicode-256color ]] ; then
+#    export TERM=rxvt
+#fi
+#
+#if [[ "${TERM}" == "xterm" ]]; then
+#  export TERM=xterm-256color
+#fi
+#
+#if [[ $TERM == "tmux-256color" && -z $TMUX ]]; then
+#  export TERM="screen-256color"
+#fi
+#
+#if [[ $TERM == "screen-256color" && -n $SSH_TTY && ! -e /usr/bin/screen ]]; then
+#  export TERM="xterm-256color"
+#fi
 
 # Disable flow control, so CTRL-S / CTRL-Q doesn't freeze the terminal
 stty -ixon -ixoff
