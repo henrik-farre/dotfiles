8c9fc7212d79740f075715af0e9169afc654910d
Henrik Farre <henrik@rockhopper.dk>
2020-07-03 20:16:09 +0200

    check of correct directory exist and is not empty

--- .config/shell.d/kubeconfig.zsh
+++ .config/shell.d/kubeconfig.zsh
@@ -1,6 +1,8 @@
 #!/bin/zsh
-if [ -d ~/.kube ]; then
+if [ -d ~/.kube/configs ]; then
+    if [ ~/.kube/configs/.(NF) ]; then
         _AVAILABLE_KUBE_CONFIGS=("${(@f)$(ls -d ~/.kube/configs/*)}")
         export KUBECONFIG=${(j#:#):-${^_AVAILABLE_KUBE_CONFIGS}}
         unset _AVAILABLE_KUBE_CONFIGS
+    fi
 fi
