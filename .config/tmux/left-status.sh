416616a2ad3d455715419600ff05421fce4ff1aa
Henrik Farre <henrik@rockhopper.dk>
2021-06-01 14:42:38 +0200

    Only show kube context if we are on production

--- .config/tmux/left-status.sh
+++ .config/tmux/left-status.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 if [[ -e /usr/bin/kubectx ]]; then
   source ~/.config/shell.d/kubeconfig.zsh
-  echo "#[fg=colour74] #[fg=colour255,bg=colour74] ⎈ $(/usr/bin/kubectx --current)"
+  local CURRENT_CONTEXT=$(/usr/bin/kubectx --current)
+  if [[ $CURRENT_CONTEXT =~ ".*prod.*" ]]; then
+    BLOCK_COLOR="colour196"
+    printf "#[fg=%s] #[fg=colour255,bg=%s] ⎈ %s" "$BLOCK_COLOR" "$BLOCK_COLOR" "$CURRENT_CONTEXT"
+  fi
 fi
