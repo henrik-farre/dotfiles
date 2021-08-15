699e7bf36a0d16d4244bc3bafa5ad50ef485aaba
Henrik Farre <henrik@rockhopper.dk>
2021-08-15 12:08:49 +0200

    Fix variable use

--- bin/move_app_to_current_workspace.sh
+++ bin/move_app_to_current_workspace.sh
@@ -9,7 +9,7 @@ APPLICATION_NAME="$2"
 WINDOW_ID=$(wmctrl -lx | grep "$WINDOW_NAME" | cut -d" " -f1)
 
 if [[ -z $WINDOW_ID ]]; then
-  exec "$2"
+  exec "$APPLICATION_NAME"
 else
   # -R <WIN> Move the window <WIN> to the current desktop, raise the window, and give it focus.
   wmctrl -i -R "$WINDOW_ID"
