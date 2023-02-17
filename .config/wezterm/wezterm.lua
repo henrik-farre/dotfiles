fd5091bf76934b9c64de1de7d1cbfb01a157b5e9
Henrik Farre <henrik@rockhopper.dk>
2023-02-17 16:51:06 +0100

    Make wezterm colors match termite

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,32 @@
 local wezterm = require 'wezterm';
+
+local base_scheme = "Tango (terminal.sexy)"
+local custom_scheme = wezterm.color.get_builtin_schemes()[base_scheme]
+-- custom_scheme.colors.ansi = {
+--   '#121212',
+--   '#ae3232',
+--   '#5b762f',
+--   '#aa9943',
+--   '#324c80',
+--   '#5f5a90',
+--   '#92b19e',
+--   '#ffffff',
+-- }
+
+-- custom_scheme.colors.brights = {
+--   '#474747',
+--   '#cb2b2b',
+--   '#89b83f',
+--   '#efef60',
+--   '#2b4f98',
+--   '#826ab1',
+--   '#a1cdcd',
+--   '#dedede',
+-- }
+
 return {
   font = wezterm.font("mononoki Nerd Font"),
   font_size = 10.5,
-  -- color_scheme = "Tango Adapted",
-  colors = {
-    foreground = "silver",
-    background = "black",
-  },
   hide_tab_bar_if_only_one_tab = true,
   default_cursor_style = "BlinkingBlock",
   cursor_blink_ease_in = "Constant",
@@ -16,5 +36,35 @@ return {
     right = 0,
     top = 0,
     bottom = 0,
-  }
+  },
+  colors = {
+    ansi = {
+      '#121212',
+      '#ae3232',
+      '#5b762f',
+      '#aa9943',
+      '#324c80',
+      '#5f5a90',
+      '#92b19e',
+      '#ffffff',
+    },
+    background = '#000000',
+    brights = {
+      '#474747',
+      '#cb2b2b',
+      '#89b83f',
+      '#efef60',
+      '#2b4f98',
+      '#826ab1',
+      '#a1cdcd',
+      '#dedede',
+    },
+    cursor_bg = '#bfceff',
+    cursor_border = '#bfceff',
+    cursor_fg = '#383a42',
+    foreground = '#383a42',
+    selection_bg = '#bfceff',
+    selection_fg = '#383a42',
+    },
+
 }
