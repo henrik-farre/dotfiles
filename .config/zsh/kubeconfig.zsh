170f366da13df8cbf6ff161750920d7ac52bc187
Henrik Farre <henrik@rockhopper.dk>
2023-03-13 14:09:12 +0100

    Removed custom kubeconfig magic

--- .config/zsh/kubeconfig.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-if [ -d ~/.kube/configs ]; then
-    if [ ~/.kube/configs/.(NF) ]; then
-        _AVAILABLE_KUBE_CONFIGS=("${(@f)$(ls -d ~/.kube/configs/*)}")
-        export KUBECONFIG=${(j#:#):-${^_AVAILABLE_KUBE_CONFIGS}}
-        unset _AVAILABLE_KUBE_CONFIGS
-    fi
-fi
