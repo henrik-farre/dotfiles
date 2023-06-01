d26b3c48f4591e6d445de9ebb57856002324e374
Henrik Farre <henrik@rockhopper.dk>
2023-06-01 09:26:58 +0200

    kube context matching and colors

--- .config/tmux/left-status.sh
+++ .config/tmux/left-status.sh
@@ -1,9 +1,14 @@
 #!/bin/zsh
-if [[ -e /usr/bin/kubectx ]]; then
-  source ~/.config/shell.d/kubeconfig.zsh
-  local CURRENT_CONTEXT=$(/usr/bin/kubectx --current)
-  if [[ $CURRENT_CONTEXT =~ ".*prod.*" ]]; then
-    BLOCK_COLOR="colour196"
+if [[ -f ~/.kube/config ]]; then
+  local CURRENT_CONTEXT=$(grep current-context: ~/.kube/config | cut -d" " -f2)
+  local BLOCK_COLOR="colour196"
+  if [[ $CURRENT_CONTEXT =~ "^.prod-.*" ]]; then
+    printf "#[fg=%s] #[fg=colour255,bg=%s] ⎈ %s" "$BLOCK_COLOR" "$BLOCK_COLOR" "$CURRENT_CONTEXT"
+  elif [[ $CURRENT_CONTEXT =~ "^.nonprod-.*" ]]; then
+    BLOCK_COLOR="colour178"
+    printf "#[fg=%s] #[fg=colour255,bg=%s] ⎈ %s" "$BLOCK_COLOR" "$BLOCK_COLOR" "$CURRENT_CONTEXT"
+  elif [[ $CURRENT_CONTEXT != '""' ]]; then
+    BLOCK_COLOR="colour34"
     printf "#[fg=%s] #[fg=colour255,bg=%s] ⎈ %s" "$BLOCK_COLOR" "$BLOCK_COLOR" "$CURRENT_CONTEXT"
   fi
 fi
