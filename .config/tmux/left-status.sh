c6a483c7027b0b4494b00fd893a3940457521939
Henrik Farre <henrik@rockhopper.dk>
2023-06-19 14:08:50 +0200

    Remove duplicated code and output k8s NS

--- .config/tmux/left-status.sh
+++ .config/tmux/left-status.sh
@@ -1,14 +1,22 @@
 #!/bin/zsh
 if [[ -f ~/.kube/config ]]; then
   local CURRENT_CONTEXT=$(grep current-context: ~/.kube/config | cut -d" " -f2)
-  local BLOCK_COLOR="colour196"
+  local CURRENT_NS=$(kubens --current)
+  local BLOCK_COLOR
+  local SHOW=n
+
   if [[ $CURRENT_CONTEXT =~ "^.prod-.*" ]]; then
-    printf "#[fg=%s] #[fg=colour255,bg=%s] ⎈ %s" "$BLOCK_COLOR" "$BLOCK_COLOR" "$CURRENT_CONTEXT"
+    SHOW=y
+    BLOCK_COLOR="colour196"
   elif [[ $CURRENT_CONTEXT =~ "^.nonprod-.*" ]]; then
+    SHOW=y
     BLOCK_COLOR="colour178"
-    printf "#[fg=%s] #[fg=colour255,bg=%s] ⎈ %s" "$BLOCK_COLOR" "$BLOCK_COLOR" "$CURRENT_CONTEXT"
   elif [[ $CURRENT_CONTEXT != '""' ]]; then
+    SHOW=y
     BLOCK_COLOR="colour34"
-    printf "#[fg=%s] #[fg=colour255,bg=%s] ⎈ %s" "$BLOCK_COLOR" "$BLOCK_COLOR" "$CURRENT_CONTEXT"
+  fi
+
+  if [[ "$SHOW" == 'y' ]]; then
+    printf "#[fg=%s] #[fg=colour255,bg=%s] ⎈ %s | %s" "$BLOCK_COLOR" "$BLOCK_COLOR" "$CURRENT_CONTEXT" "$CURRENT_NS"
   fi
 fi
