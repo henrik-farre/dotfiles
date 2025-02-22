cad926d0865d3efce71e3c8a4fe418f2e371268b
Henrik Farre <henrik@rockhopper.dk>
2025-02-22 13:58:58 +0100

    Cleanup git aliases and add new functions
    * Use https://github.com/junegunn/fzf-git.sh

--- .config/zsh/alias.git.zsh
+++ .config/zsh/alias.git.zsh
@@ -1,3 +1,16 @@
 # Feature branch
 alias gfb="git checkout -b"
 compdef _git gfb=git-checkout
+
+# https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/git/git.plugin.zsh
+# Aliases
+alias gst='git status'
+compdef _git gst=git-status
+# Fix typo "gs" when I ment "gst"
+alias gs='git status'
+compdef _git gs=git-status
+alias gd='git diff'
+compdef _git gd=git-diff
+# alias gco='git checkout'
+# compdef _git gco=git-checkout
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
--- .config/zsh/functions.git-fzf.zsh
+++ .config/zsh/functions.git-fzf.zsh
@@ -1,45 +1,10 @@
-# https://github.com/junegunn/fzf/wiki/Examples#git
-function git-checkout-local-branch() {
-  local tags branches target
-  branches=$(
-    git --no-pager branch --all \
-      --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" \
-    | sed '/^$/d') || return
-  tags=$(
-    git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}') || return
-  target=$(
-    (echo "$branches"; echo "$tags") |
-    fzf --no-hscroll --no-multi -n 2 \
-        --ansi --preview="git --no-pager log -150 --pretty=format:%s '..{2}'") || return
-  git checkout $(awk '{print $2}' <<<"$target" )
+# https://github.com/junegunn/fzf-git.sh
+# Pick branch
+function gcob() {
+  _fzf_git_each_ref --no-multi | xargs git checkout
 }
 
-# Based on https://seb.jambor.dev/posts/improving-shell-workflows-with-fzf/
-function git-create-branch-from-jira() {
-  branch_name=$(
-    jira tasks-with-id-and-description |
-    fzf \
-      --with-nth=1 \
-      --delimiter='\t' \
-      --preview='echo -e {2}' \
-      --preview-window=top:wrap |
-    cut -f1 |
-    awk '{printf "%s", $1}'
-  )
-
-  if [ -n "$branch_name" ]; then
-    git checkout -b "$branch_name"
-  fi
-}
-
-# Based on https://seb.jambor.dev/posts/improving-shell-workflows-with-fzf/ and
-# https://github.blog/2021-03-11-scripting-with-github-cli/#combine-gh-with-other-tools
-function git-pr-checkout() {
-  local pr_number
-
-  pr_number=$(gh pr list -L100 | fzf | cut -f1)
-
-  if [ -n "$pr_number" ]; then
-    gh pr checkout "$pr_number"
-  fi
+# Pick files to commit
+function gcif() {
+  _fzf_git_files | xargs git ci
 }
